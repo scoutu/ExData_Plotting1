186e813e03b92f595695042e088c5e95dc2edc6f
scoutu <sandra.coutu@aimia.com>
2016-04-23 07:28:21 -0400

    My assignement week1

---
+++ plot1.R
@@ -0,0 +1,12 @@
+setwd("C:/Users/Sandra/Documents/Coursera/Exploratory Data Analysis/Week1 project/Data")
+
+dataFile <- "household_power_consumption.txt" 
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".") 
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,] 
+
+
+#str(subSetData) 
+globalActivePower <- as.numeric(subSetData$Global_active_power) 
+png("plot1.png", width=480, height=480) 
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)") 
+dev.off() 
